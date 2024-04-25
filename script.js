69b044b37a2ec697d0f5fa8e709a288eb9e85e37
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-25 15:06:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
-    //Write your code here
-
+	 // Get the form element using its ID
+    const form = document.getElementById("form1");
+    // Get the values of the first and last name from the form elements
+//form.elements = gives access to collection of all form controls (like input fields, buttons, etc.) within that form
+    const firstName = form.elements["fname"].value;//getting particular fname formControl & itsValue
+    const lastName = form.elements["lname"].value;
+	const fullName = firstName + " " + lastName;
+	alert(fullName)   
 }
+   
